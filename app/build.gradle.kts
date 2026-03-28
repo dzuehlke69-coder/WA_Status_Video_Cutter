e6ab3cc329e2158f430eda07d2ec1c9003d9090f
dzuehlke69-coder <dzuehlke69@gmail.com>
2026-03-28 11:42:01 +0100

    Update version to 2.0.1 and scanner fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dzuehlke69.wa_status_video_cutter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 200
-        versionName = "2.0.0"
+        versionCode = 201
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
